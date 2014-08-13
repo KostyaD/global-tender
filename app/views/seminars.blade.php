b8fd10dcd112ae76dab9cf3de3ccfad5e334e9fd
KostyaD <thedamaxstudio@gmail.com>
2014-08-13 15:40:57 +0400

    fixes

--- app/views/seminars.blade.php
+++ app/views/seminars.blade.php
@@ -13,16 +13,19 @@ $semArray = array(
         'piter223' => array('Санкт-Петербург', '15 августа', '', 'http://ved69.ru/images/Spb.jpg'),
         'yl223' => array('Ульяновск', '08 августа', '', 'http://xn----7sbaba0dyat1arg.xn--p1ai/image/3panorama-1.jpg'),
         //'vladimir223' => array('Владимир', '11 августа', '', 'http://www.zapoved.ru/photos/9bab/db77/834c/1083/95b7/ee05/50a8/bbba/large.jpg?2880811'),
-        'petroz223' => array('Петрозаводск', '28 августа', '', 'img/city/petroz.jpg'),
+        'petroz223' => array('Петрозаводск', '28 августа', '', 'http://www.vokrugsveta.ru/encyclopedia/images/e/e4/Petrozavodsk_Naberezhnaya.jpg'),
         'vor223' => array('Воронеж', '22 августа', '', 'http://bestmaps.ru/files/content_images/20130204134915.jpg'), 
         'cheb223' => array('Чебоксары', '29 августа', '', 'http://www.mrtrans.ru/uploads/tiny/ceb.jpg'), 
+        'birb223' => array('Саранск', '05 сентября', '', 'http://www.udikov.ru/wp-content/uploads/2012/11/Saransk.jpg'), 
         'birb223' => array('Биробиджан', '15 сентября', '', 'http://biradm.ru/files/potr/pict/2012_2.jpg'), 
-        'xab223' => array('Хабаровск', '19 сентября', '', 'http://organum-visus.com/sites/default/files/styles/1024x768/public/news_mini_top_img/khabarovsk-organum-visus-eyenews-120712g.jpeg?itok=C7U8OkzE&slideshow=true&slideshowAuto=false&slideshowSpeed=4000&speed=350&transition=fade'),       
+        'xab223' => array('Хабаровск', '15 сентября', '', 'http://organum-visus.com/sites/default/files/styles/1024x768/public/news_mini_top_img/khabarovsk-organum-visus-eyenews-120712g.jpeg?itok=C7U8OkzE&slideshow=true&slideshowAuto=false&slideshowSpeed=4000&speed=350&transition=fade'),       
+        'vlad223' => array('Владивосток', '16 сентября', '', 'http://s.pikabu.ru/post_img/big/2013/10/23/5/1382508326_1790634624.jpg'), 
         /*
         'city223' => array('Ярославль', '25 июля 2014', '', 'http://'),         
         */
         ),
     '44'  => array(
+        'rostov44' => array('Ростов-на-Дону', '12 сентября', '', 'http://upload.wikimedia.org/wikipedia/ru/0/0f/%D0%9D%D0%B0%D0%B1%D0%B5%D1%80%D0%B5%D0%B6%D0%BD%D0%B0%D1%8F_%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2%D0%B0-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83.jpg'), 
         ),
     'nal'  => array(
         ),
@@ -55,13 +58,13 @@ $semArray = array(
                         ?>
                     </ul>
 
-                    <!--<h3 class="seminar-title">«Контрактная система в сфере закупок товаров, работ, услуг для обеспечения государственных и муниципальных нужд». Последние обновления в 44 Ф.З. Специальный семинар для поставщиков.</h3>-->
+                    <h3 class="seminar-title">«Контрактная система в сфере закупок товаров, работ, услуг для обеспечения государственных и муниципальных нужд». Последние обновления в 44 Ф.З. Специальный семинар для поставщиков.</h3>
                     <ul class="seminars">
                         <?php
                             foreach($semArray[44] as $val => $sem) {
                                 echo   "<li class=\"seminar\">
                                             <div class=\"seminar-back\" style=\"background-image: url(".$sem[3].");\"></div>
-                                            <a href=\"seminar?act=223&sem=".$val."\">
+                                            <a href=\"seminar?act=44&sem=".$val."\">
                                                 <span class=\"sem-date\">".$sem[1]." <span class=\"year\">'14</span></span>
                                                 <span class=\"place\">".$sem[0]."</span>
                                                 <!--<span>Вертол, 5 улица</span>-->
